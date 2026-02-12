13654c457e8b98e1fdaf4d947a3bc9a3c872c0e8
Yusuf219 <115668129+Yusuf219@users.noreply.github.com>
2026-02-12 11:00:33 +0000

    Update reddit_client.py
    Added config launcher and Reddit client

--- reddit_client.py
+++ reddit_client.py
@@ -1,11 +1,14 @@
 import praw
-from config import CLIENT_ID, CLIENT_SECRET, USERNAME, PASSWORD, USER_AGENT
+from .config import Settings
 
-def get_reddit_instance():
-    return praw.Reddit(
-        client_id=CLIENT_ID,
-        client_secret=CLIENT_SECRET,
-        username=USERNAME,
-        password=PASSWORD,
-        user_agent=USER_AGENT,
+def create_reddit(settings: Settings) -> praw.Reddit:
+    reddit = praw.Reddit(
+        client_id=settings.client_id,
+        client_secret=settings.client_secret,
+        username=settings.username,
+        password=settings.password,
+        user_agent=settings.user_agent,
     )
+    # Touch an endpoint to validate creds early
+    _ = reddit.user.me()
+    return reddit
